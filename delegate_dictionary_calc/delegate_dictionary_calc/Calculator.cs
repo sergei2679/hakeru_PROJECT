0ae2bb6b211211cb2c77fb5fdb11aca20f518898
sergei2679 <60327654+sergei2679@users.noreply.github.com>
2020-05-17 17:11:23 +0300

    c

---
+++ delegate_dictionary_calc/delegate_dictionary_calc/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace delegate_dictionary_calc
+{
+    class Calculator
+    {
+        static Dictionary<string, Func<double, double, double>> _operations;
+
+        public Calculator()
+        {
+            // with lambda
+            _operations =
+               new Dictionary<string, Func<double, double, double>>
+               {
+                    { "+", (x, y) => x + y },
+                    { "-", (x, y) => x - y },
+                    { "*", (x, y) => x * y },
+                    { "/", (x, y) => x / y }
+               };
+
+        }
+
+        // with method
+        //private static double DoDivision(double x, double y) { return x / y; }
+        //private static double DoMultiplication(double x, double y) { return x * y; }
+        //private static double DoSubtraction(double x, double y) { return x - y; }
+        //private static double DoAddition(double x, double y) { return x + y; }
+
+
+        public double PerformOperation(string op, double x, double y)
+        {
+            if (!_operations.ContainsKey(op))
+                throw new ArgumentException(string.Format("Operation {0} is invalid", op), "op");
+            return _operations[op](x, y);
+        }
+
+
+        public void DefineOperation(string op, Func<double, double, double> body)
+        {
+            if (_operations.ContainsKey(op))
+                throw new ArgumentException(string.Format("Operation {0} already exists", op), "op");
+            _operations.Add(op, body);
+        }
+
+    }
+}
---
+++ delegate_dictionary_calc/delegate_dictionary_calc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace delegate_dictionary_calc
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            //ומעבירים למחלקה SWITH CASE מתפטרים בתוכנית הראשית מ
+            var add = calc.PerformOperation("+", 2, 7);
+            Console.WriteLine("add operation = " + add);
+
+
+            //   אם נרצה מחר להוסיף למחלקה המקומפלת עוד פעולות זה שינוי די יקר,
+            // DLL כי צריך לקמפל מחדש את המודול
+
+            calc.DefineOperation("mod", (x, y) => x % y);        // כך נוכל להוסיף פעולות בלי לקמפל מחדש את המחלקה
+            var mod = calc.PerformOperation("mod", 4.0, 2.0);    // לדוגמא מוסיפים פעולת מודולו
+            Console.WriteLine("mod operation = " + mod);
+
+
+            calc.DefineOperation("-", (x, y) => x - y);  // במקרה הזה פעולה כבר קיימת , יזרוק שגיאה
+
+
+            Console.ReadLine();
+        } 
+    }
+}
