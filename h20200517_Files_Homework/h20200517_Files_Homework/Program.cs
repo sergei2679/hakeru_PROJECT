5455459f9a1614d9df4d2f3c191754b94a46abd5
sergei2679 <60327654+sergei2679@users.noreply.github.com>
2020-05-27 12:52:32 +0300

    Update Program.cs

--- h20200517_Files_Homework/h20200517_Files_Homework/Program.cs
+++ h20200517_Files_Homework/h20200517_Files_Homework/Program.cs
@@ -5,10 +5,6 @@ namespace h20200517_Files_Homework
 {
     class Program
     {
-        public static readonly FileInfo file = new FileInfo(@"C:\Test\myFile.dat");
-        //private static string file1 = @"C:\Test\‏‏source1";
-        //private static string file2 = @"C:\Test\‏‏source2";
-        //private static FileInfo mergedFile = new FileInfo(@"C:\Test\‏‏destination.txt");
 
         static void Main(string[] args)
         {
@@ -48,59 +44,12 @@ namespace h20200517_Files_Homework
 
             Homework.ReadMax();
             Homework.ReadMin();
-
-
-            //------------------------- Merge ------------------------------------
-
-         
+            
 
             Console.ReadLine();
         }
 
         
     }
-
-    public static class ExtensionMethods
-    {
-        public static byte[] GetBytes(this string s)
-        {
-            return System.Text.ASCIIEncoding.UTF8.GetBytes(s);
-        }
-        public static void Write(this string s, FileInfo file)
-        {
-            byte[] sBytes = System.Text.ASCIIEncoding.UTF8.GetBytes(s);
-            using (Stream stream = file.OpenWrite())
-            {
-                stream.Write(sBytes, 0, sBytes.Length);
-            }
-        }
-        public static void Write(this string s, string file)
-        {
-            s.Write(new FileInfo(file));
-        }
-
-        public static void Print(this FileInfo fileMerged)
-        {
-            //FileInfo fileInfo = new FileInfo(path);
-
-            using (Stream stream = fileMerged.OpenRead())
-            {
-                byte[] buffer = new byte[5];
-                int actuallyRead = stream.Read(buffer, 0, buffer.Length);
-                string s = System.Text.ASCIIEncoding.UTF8.GetString(buffer, 0, actuallyRead);
-                Console.WriteLine(s);
-                actuallyRead = stream.Read(buffer, 0, buffer.Length);
-                s = System.Text.ASCIIEncoding.UTF8.GetString(buffer, 0, actuallyRead);
-                Console.WriteLine(s);
-                Console.WriteLine(actuallyRead);
-
-            }
-        }
-    }
-
-    //public interface IWritable
-    //{
-    //    public void Write(Stream stream);
-    //}
 
 }
